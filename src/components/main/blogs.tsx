8fc5cbc70d8e068000a6ac5fe351c02d88279b29
Mateusz Zwierzchowski <zwierzchowski.mateo@gmail.com>
2024-02-26 18:20:58 +0100

    update

--- src/components/main/blogs.tsx
+++ src/components/main/blogs.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { collection, onSnapshot, doc, deleteDoc } from "firebase/firestore";
+import { collection, onSnapshot, doc, deleteDoc, updateDoc } from "firebase/firestore";
 import { db, auth } from "../../config/firebase";
 import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,14 +7,17 @@ import "../../styles/Blogs.css";
 import { faEdit, faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 import { faGear } from "@fortawesome/free-solid-svg-icons";
 
+//ZROBIC Z LICZNIK CZRWNOY GDY ZA DUZO
+
 
 type Blog = {
   tit: string;
   des: string;
   aut: string;
   id: string;
-  BlogId:string;
-  settingsMode:boolean;
+  BlogId: string;
+  settingsMode: boolean;
+  editMode: boolean;
 };
 
 type BlogsProps = {
@@ -24,11 +27,20 @@ type BlogsProps = {
 const Blogs = ({ isAuth }: BlogsProps) => {
   const [blogs, setBlogs] = useState<Blog[]>([]);
 
+  const [updatedTitle, setUpdatedTitle] = useState("");
+  const [updatedDescription, setUpdatedDescription] = useState("");
+
+  const [n, setN] = useState("licznik")
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, "blogs"), (snapshot) => {
       const updatedBlogs: Blog[] = [];
       snapshot.forEach((doc) => {
-        updatedBlogs.push({ id: doc.id, ...doc.data(), settingsMode: false } as Blog);
+        updatedBlogs.push({
+          id: doc.id,
+          ...doc.data(),
+          settingsMode: false,
+          editMode: false,
+        } as Blog);
       });
       setBlogs(updatedBlogs);
     });
@@ -38,19 +50,40 @@ const Blogs = ({ isAuth }: BlogsProps) => {
 
   const handleDelete = async (id: string) => {
     try {
-      await deleteDoc(doc(db, "blogs", id))
+      await deleteDoc(doc(db, "blogs", id));
     } catch (error) {
       console.error("Error while deleting blog:", error);
     }
   };
 
-  const handleEdit = (id: string) => {
-    console.log("Editing blog with id:", id);
-  };
-
   const toggleSettings = (index: number) => {
     const updatedBlogs = [...blogs];
     updatedBlogs[index].settingsMode = !updatedBlogs[index].settingsMode;
+    setBlogs(updatedBlogs);
+  };
+
+  const toggleEdit = (index: number) => {
+    const updatedBlogs = [...blogs];
+    updatedBlogs[index].editMode = !updatedBlogs[index].editMode;
+    setBlogs(updatedBlogs);
+  };
+
+  const editSubmit = async (index: number) => {
+    const blogToUpdate = blogs[index];
+    try {
+      await updateDoc(doc(db, "blogs", blogToUpdate.BlogId), {
+        tit: updatedTitle || blogToUpdate.tit, 
+        des: updatedDescription || blogToUpdate.des, 
+      });
+    } catch (error) {
+      console.error("Error while updating blog:", error);
+    }
+
+
+    const updatedBlogs = [...blogs];
+    updatedBlogs[index].editMode = false;
+    updatedBlogs[index].tit = updatedTitle || blogToUpdate.tit;
+    updatedBlogs[index].des = updatedDescription || blogToUpdate.des;
     setBlogs(updatedBlogs);
   };
 
@@ -60,18 +93,65 @@ const Blogs = ({ isAuth }: BlogsProps) => {
         <div key={index} className="cont">
           <p className="author">{blog.aut}</p>
           <div className="k">
-            <FontAwesomeIcon icon={faGear} className="gear" onClick={() => toggleSettings(index)} />
-            <h3 className="title">{blog.tit}</h3>
-            {blog.settingsMode && isAuth && auth.currentUser?.uid.toString() === blog.id.toString() ? (
+            {isAuth &&
+            auth.currentUser?.uid.toString() === blog.id.toString() ? (
+              <FontAwesomeIcon
+                icon={faGear}
+                className="gear"
+                onClick={() => toggleSettings(index)}
+              />
+            ) : (
+              <div></div>
+            )}
+            {blog.editMode === true ? (
+              <input placeholder={blog.tit} className="editTit" type="text" onChange={(e) => setUpdatedTitle(e.target.value)}/>
+              ):(
+              <h3 className="title">{blog.tit}</h3>
+            )}
+            {blog.settingsMode &&
+            isAuth &&
+            auth.currentUser?.uid.toString() === blog.id.toString() ? (
               <div className="others">
-                <FontAwesomeIcon icon={faEdit} className="emojis" onClick={() => handleEdit(blog.BlogId)} />
-                <FontAwesomeIcon icon={faTrashAlt} className="emojis" onClick={() => handleDelete(blog.BlogId)} />
+                <FontAwesomeIcon
+                  icon={faEdit}
+                  className="emojis"
+                  id="ed"
+                  onClick={() => toggleEdit(index)}
+                />
+                <FontAwesomeIcon
+                  icon={faTrashAlt}
+                  className="emojis"
+                  id="tr"
+                  onClick={() => handleDelete(blog.BlogId)}
+                />
               </div>
             ) : (
               <div></div>
             )}
           </div>
-          <p className="description">{blog.des}</p>
+          {blog.editMode ? (
+            <textarea
+              placeholder={blog.des}
+              className="editDes"
+              onChange={(e) => setUpdatedDescription(e.target.value)}
+            />
+          ) : (
+            <p className="description">{blog.des}</p>
+          )}
+          
+          {blog.editMode ?(
+            <h3 className={n}>{`${updatedDescription.length} / 500`}</h3>
+            ):(
+             <div></div>
+            )
+            }
+          {blog.editMode ? (
+            <button className="confirm" onClick={() => editSubmit(index)}>
+              Confirm
+            </button>
+          ) : (
+            <div></div>
+          )}
         </div>
       ))}
     </div>
--- src/components/main/create.tsx
+++ src/components/main/create.tsx
@@ -38,7 +38,7 @@ const CreateBlogs = (props:blogsTypes) => {
         try{
             const currentUser = auth.currentUser;
           
-            if (currentUser && description.trim().length >= 1 && title.trim().length >= 1 ){
+            if (currentUser && description.trim().length >= 1 && title.trim().length >= 1 && description.trim().length <= 500 && title.trim().length <= 25){
             const displayName = currentUser.displayName;
             
             const docRef = await addDoc(collection(db, "blogs"), {
@@ -69,8 +69,15 @@ const CreateBlogs = (props:blogsTypes) => {
             <div className="inputsBlogs">
             <label htmlFor="title">Title:</label>
             <input type="text" placeholder="Write your title here..." name="title" onChange={(e) => setTitle(e.target.value)} value={title}/>
+            <h3 className="licznik">{`${title.length} / 25`}</h3>
             <label htmlFor="title">Description:</label>
             <textarea id="description" name="description" rows={4} cols={50} placeholder="Write your description here..." onChange={(e) => setDescription(e.target.value)} value={description}></textarea>
+            {description.length <= 500 ?(
+            <h3 className="licznik">{`${description.length} / 500`}</h3>
+            ):(
+              <h3 className="licznikWrong">{`${description.length} / 500`}</h3>
+            )
+            }
             </div>
             <div className="buttonsBlogs">
                 <button id="submit" onClick={handleSubmit}>Submit</button>
